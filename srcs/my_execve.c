6992b32b8c24f2cb047cd55bba4d47b7745a5b1d
irn66 <126900447+idumenil@users.noreply.github.com>
2023-12-13 18:01:54 +0100

    Update my_execve.c
    Suppression de la phrase de debogage concernant le code de sortie pour avoir uniquement le code comme dans bash

--- srcs/my_execve.c
+++ srcs/my_execve.c
@@ -54,18 +54,12 @@ int	my_execve(char **args, t_data *datas)
 	}
 	else
 	{
-		//int status;
-		//wait(&status);
-
 		waitpid(pid, &datas->cmd_ret, 0);
 		datas->exit_status = WEXITSTATUS(datas->cmd_ret);
-		printf("Code sortie : %d\n", datas->exit_status); //debug
-
 		if (WIFEXITED(datas->cmd_ret))
-			printf("Le processus fils s'est terminé normalement avec le code de sortie : %d\n", WEXITSTATUS(datas->cmd_ret));
+			printf("%d\n", WEXITSTATUS(datas->cmd_ret));
 		else if (WIFSIGNALED(datas->cmd_ret))
-			printf("Le processus fils s'est terminé à cause du signal : %d\n", WTERMSIG(datas->cmd_ret));
-
+			printf("%d\n", WTERMSIG(datas->cmd_ret));
 	}
 	free(prog_path);
 	return (0);
